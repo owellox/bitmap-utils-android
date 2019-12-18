89567e7e4c561cd53bec927c16bbfecf51c2221f
Chan Teck Wei <34728894+chantw@users.noreply.github.com>
2019-12-18 21:23:14 +0800

    Add code documentation

--- bitmap-utils-ktx/src/main/java/com/owellox/bitmap/Bitmap.kt
+++ bitmap-utils-ktx/src/main/java/com/owellox/bitmap/Bitmap.kt
@@ -4,6 +4,18 @@ import android.graphics.Bitmap
 import androidx.core.graphics.scale
 import kotlin.math.min
 
+/**
+ * Crops and returns exactly the center of the Bitmap.
+ *
+ * @param sideLength Optional. The target length of all sides of the square.
+ * @param filter Whether or not bilinear filtering should be used when scaling the bitmap. If this
+ * is true then bilinear filtering will be used when scaling which has better image quality at the
+ * cost of worse performance. If this is false then nearest-neighbor scaling is used instead which
+ * will have worse image quality but is faster. Recommended default is to set filter to 'true' as
+ * the cost of bilinear filtering is typically minimal and the improved image quality is
+ * significant.
+ * @return The new cropped, square Bitmap.
+ */
 fun Bitmap.cropCenterSquare(sideLength: Int? = null, filter: Boolean = true): Bitmap {
     val minLength = min(width, height)
     val left = ((width / 2f) - (minLength / 2f)).toInt()
